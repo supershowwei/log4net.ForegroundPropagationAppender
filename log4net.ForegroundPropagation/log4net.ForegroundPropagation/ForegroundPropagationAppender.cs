c9546b95cd75b814dfbb1984d67430e8ffa379dc
johnny <johnny@wantgoo.com>
2016-10-13 14:26:21 +0800

    init

---
+++ log4net.ForegroundPropagation/log4net.ForegroundPropagation/ForegroundPropagationAppender.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using log4net.Core;
+
+namespace log4net.Appender
+{
+    public delegate void PropagationHandler(DateTime timeStamp, Level level, string message);
+
+    public class ForegroundPropagationAppender : AppenderSkeleton
+    {
+        public PropagationHandler Propagation { private get; set; }
+
+        protected override void Append(LoggingEvent loggingEvent)
+        {
+            if (this.Propagation != null)
+            {
+                this.Propagation(loggingEvent.TimeStamp, loggingEvent.Level, loggingEvent.RenderedMessage);
+            }
+        }
+    }
+}
